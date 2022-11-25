6fde1cbd8332b600be3a289444c05e8e6f2e07e3
João Vitor Marques <jvsilvam@outlook.com>
2022-11-24 22:45:30 -0300

    init: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'unused-imports'
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+
+import { Home } from './screens'
+
+export default function App () {
+  return (
+    <>
+      <StatusBar hidden />
+      <Home />
+    </>
+  )
+}
