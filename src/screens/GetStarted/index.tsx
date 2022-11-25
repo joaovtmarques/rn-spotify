aa872216a4eb62d5dda434c7e4288c60bcae720a
João Vitor Marques <jvsilvam@outlook.com>
2022-11-25 10:15:32 -0300

    refactor: change button on get started screen

--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,13 +1,8 @@
-import {
-  ImageBackground,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { ImageBackground, SafeAreaView, Text, View } from 'react-native';
 
 import backgroundImg from '../../assets/get-started-background.png';
 import Logo from '../../assets/logo.svg';
+import { Button } from '../../components';
 
 export function GetStarted() {
   return (
@@ -27,11 +22,7 @@ export function GetStarted() {
               'Lorem ipsum dolor sit amet,\n consectetur adipiscing elit. Sagittis enim\n purus sed phasellus. Cursus ornare id\n scelerisque aliquam.'
             }
           </Text>
-          <TouchableOpacity className="h-[92px] w-full bg-primaryGreen rounded-[30px] items-center justify-center mt-9">
-            <Text className="text-lightGrey text-center text-[22px] font-sBold">
-              Get Started
-            </Text>
-          </TouchableOpacity>
+          <Button title="Get Started" />
         </View>
       </SafeAreaView>
     </ImageBackground>
